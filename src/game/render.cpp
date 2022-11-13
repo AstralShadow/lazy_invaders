b05ef8a5e862a3ea67511c088046170eeb2845b9
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2022-11-13 12:49:57 +0200

    Rendering

--- src/game/render.cpp
+++ src/game/render.cpp
@@ -1,4 +1,5 @@
 #include "game/game.hpp"
+#include "game/data.hpp"
 #include "core/core.hpp"
 #include <SDL2/SDL_render.h>
 
@@ -10,6 +11,29 @@ void game::render()
         SDL_SetRenderDrawColor(rnd, 0, 0, 0, 255);
         SDL_RenderClear(rnd);
 
+        SDL_SetRenderDrawColor(rnd, 0, 255, 0, 255);
+        for(auto& player : game::players) {
+               SDL_Rect area {
+                        (int) player.pos.x,
+                        (int) player.pos.y,
+                        64,
+                        64
+               }; 
+
+               SDL_RenderFillRect(rnd, &area);
+        }
+
+        SDL_SetRenderDrawColor(rnd, 127, 0, 0, 255);
+        for(auto& enemy : game::enemies) {
+               SDL_Rect area {
+                        (int) enemy.pos.x,
+                        (int) enemy.pos.y,
+                        64,
+                        64
+               }; 
+
+               SDL_RenderFillRect(rnd, &area);
+        }
 
 
         SDL_RenderPresent(rnd);
